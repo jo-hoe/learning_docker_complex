cfc098b194e496c71c27457e3b617b8ba1cf9497
Johannes Hönger <johannes.hoenger@sap.com>
2020-09-11 23:06:07 +0200

    added second page

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return(
+        <div>
+            Test page to check routing in docker
+            <Link to="/">Go back to start page</Link>
+        </div>
+    );
+};
